2631a0d65c6e90f866047233cb8ce0ed301503b3
Arthur Fücher <arthurfucher@gmail.com>
2020-12-10 22:29:33 -0300

    atualiza angular pra v11

--- AeraWebApp/src/app/cliente/cliente.component.spec.ts
+++ AeraWebApp/src/app/cliente/cliente.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ClienteComponent } from './cliente.component';
 
@@ -6,7 +6,7 @@ describe('ClienteComponent', () => {
   let component: ClienteComponent;
   let fixture: ComponentFixture<ClienteComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ClienteComponent ]
     })
