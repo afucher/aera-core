fc9a62bb34f21268e6f9e34d124289e043babeea
Arthur Fücher <arthurfucher@gmail.com>
2021-02-04 23:18:21 -0300

    permite salvar e criar novo cliente com um botao apenas

--- AeraWebApp/src/app/cliente/cliente-novo/cliente-novo.component.ts
+++ AeraWebApp/src/app/cliente/cliente-novo/cliente-novo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { PoDynamicFormField, PoNotificationService } from '@po-ui/ng-components';
 import { ClienteService } from 'src/app/cliente.service';
 import { Cliente, clienteFields } from 'src/app/models/cliente';
@@ -18,18 +19,33 @@ export class ClienteNovoComponent implements OnInit {
 
   constructor(
     public poNotification: PoNotificationService,
-    private clienteService: ClienteService) { }
+    private clienteService: ClienteService,
+    private router: Router) { }
 
     formataHorário(horario: string) {
       if (horario && horario.length === 4) { return horario.slice(0, 2) + ':' + horario.slice(2); }
       return horario;
     }
 
-  onSubmit(e) {
+  salvar(e) {
     this.clienteService
       .criar({...e.value, hora_nascimento: this.formataHorário(e.value.hora_nascimento)})
       .subscribe(
-        (_) => this.poNotification.success('Cliente criado com sucesso!'),
+        (c) => {
+          this.poNotification.success('Cliente criado com sucesso!');
+          this.router.navigate([`/clientes/${c.id}/detalhes`])
+        },
+        (err) => this.poNotification.error((err.error as string).split('/n')[0])
+      );
+   }
+
+   salvarECriarNovo(e) {
+    this.clienteService
+      .criar({...e.value, hora_nascimento: this.formataHorário(e.value.hora_nascimento)})
+      .subscribe(
+        (c) => {
+          this.poNotification.success('Cliente criado com sucesso!');
+        },
         (err) => this.poNotification.error((err.error as string).split('/n')[0])
       );
    }
