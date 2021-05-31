e9d1ee171d4f02db7f3e3cb26959f50df38af3a4
Arthur Fücher <arthurfucher@gmail.com>
2021-05-30 23:18:45 -0300

    altera retorno da api de professores

---
+++ server/aera/Controllers/ProfessorDTO.cs
@@ -0,0 +1,8 @@
+namespace aera_core.Controllers
+{
+    public class ProfessorDTO
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+    }
+}
--- server/aera/Controllers/ProfessoresController.cs
+++ server/aera/Controllers/ProfessoresController.cs
@@ -19,40 +19,18 @@ namespace aera_core.Controllers
         }
 
         [HttpGet]
-        public POUIListResponse<ClienteDTO> Get([FromQuery] int page, [FromQuery] int pageSize)
+        public POUIListResponse<ProfessorDTO> Get([FromQuery] int page, [FromQuery] int pageSize)
         {
             var opções = new OpçõesBusca
             {
                 Página = page < 1 ? 1 : page,
                 LimitePágina = pageSize == 0 ? 10 : pageSize,
             };
-            var clientes = _professoresServiço.ObterTodos(opções);
-            var clientesDTO = clientes.Select(cliente => new ClienteDTO
-            {
-                id = cliente.Id,
-                nome = cliente.Nome,
-                address1 =  cliente.address1,
-                address2 =  cliente.address2,
-                address3 =  cliente.address3,
-                celular = cliente.Celular,
-                cep = cliente.CEP,
-                cidade = cliente.Cidade,
-                cpf = cliente.CPF,
-                data_nascimento = cliente.DataNascimento?.ToString("yyyy-MM-dd"),
-                profissao = cliente.Profissão,
-                professor = cliente.ÉProfessor,
-                email = cliente.Email,
-                estado = cliente.Estado,
-                hora_nascimento = cliente.HorárioNascimento?.ToString(@"hh\:mm"),
-                local_nascimento = cliente.LocalNascimento,
-                nivel_educacao = cliente.NívelEducação,
-                observacao = cliente.Observação,
-                telefone = cliente.Telefone,
-                telefone_comercial = cliente.TelefoneComercial
+            var professores = _professoresServiço.ObterTodos(opções);
 
-            }).ToArray();
-
-            return new POUIListResponse<ClienteDTO>(clientesDTO, clientes.TemMaisItens);
+            return new POUIListResponse<ProfessorDTO>(
+            professores.Select(c => new ProfessorDTO{id = c.Id, nome = c.Nome}).ToArray(), 
+                professores.TemMaisItens);
         }
         
         [HttpGet("{id}")]
