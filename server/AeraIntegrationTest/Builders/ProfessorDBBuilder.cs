faa7a4d4fb59d03d7c147f967cd0f29e5ca9b2fe
Arthur Fücher <arthurfucher@gmail.com>
2021-06-20 19:59:07 -0300

    cria builder

---
+++ server/AeraIntegrationTest/Builders/ProfessorDBBuilder.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using aera_core.Persistencia;
+using Bogus;
+
+namespace AeraIntegrationTest.Builders
+{
+    public static class ProfessorDBBuilder
+    {
+        public static ClienteDB Generate()
+        {
+            var professorFaker = new Faker<ClienteDB>()
+                .RuleFor(p => p.name, f => f.Name.FullName())
+                .RuleFor(p => p.teacher, _ => true);
+            return professorFaker.Generate();
+        }
+    }
+}
