772352864bf1adf307b183bade0bad038c1938cc
Arthur Fücher <arthurfucher@gmail.com>
2021-06-29 20:55:07 -0300

    adiciona autenticacao nos testes de Api

--- server/AeraIntegrationTest/Testes/Controllers/BaseTesteApi.cs
+++ server/AeraIntegrationTest/Testes/Controllers/BaseTesteApi.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Net.Http.Headers;
 using aera_core.Persistencia;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
@@ -8,11 +9,14 @@ namespace AeraIntegrationTest
     public class BaseTesteApi : BaseTesteBanco
     {
         protected HttpClient _httpClient;
+        protected string _token;
 
         [SetUp]
         public void ConfiguraHttpClient()
         {
             _httpClient = AmbienteDeTestes.Factory.CreateClient();
+            _token = Helpers.obterAccessToken(_httpClient).Result;
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
         }
     }
 }
