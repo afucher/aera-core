4da53d84994b31beda9598da67493f4f21c401e2
Arthur Fücher <arthurfucher@gmail.com>
2021-07-04 00:46:54 -0300

    tentativa de adicionar login

--- AeraWebApp/src/app/app-routing.module.ts
+++ AeraWebApp/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientListComponent } from './client-list/client-list.component';
@@ -9,11 +10,12 @@ import { TurmaEditComponent } from './turma/turma-edit/turma-edit.component';
 import { TurmaNovaComponent } from './turma/turma-nova/turma-nova.component';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoEditComponent } from './curso/curso-edit/curso-edit.component';
-import { CursoNovoComponent } from './curso/curso-novo/curso-novo.component'
-import { CursoResumoComponent } from './curso/curso-resumo/curso-resumo.component'
+import { CursoNovoComponent } from './curso/curso-novo/curso-novo.component';
+import { CursoResumoComponent } from './curso/curso-resumo/curso-resumo.component';
 import { PagamentoListComponent } from './pagamento/pagamento-list/pagamento-list.component';
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path: 'clientes', component: ClientListComponent},
   {path: 'clientes/novo', component: ClienteNovoComponent},
   {path: 'clientes/:id/alterar', component: ClienteAlteraComponent},
